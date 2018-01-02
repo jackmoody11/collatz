c5731e7ecaeb5ae693cf3cb2723606b6900c7f7a
Jack Moody <jacklaytonmoody@gmail.com>
2018-01-01 19:11:15 -0500

    Initial commit

---
+++ collatz.m
@@ -0,0 +1,20 @@
+function collatz(N)
+A = zeros(1,N);
+    for j = 1:N
+    c(1) = j; 
+    i=2;
+    n=j;
+        while ~(n==1)
+              if mod(n,2) ~= 0
+                  n = 3*n+1;
+              else
+                  n = n/2;
+              end
+              c(i) = n;
+              i=i+1;
+        end
+    [rows,cols] = size(c);
+    A(j) = cols;
+    end
+    plot(A)
+end
